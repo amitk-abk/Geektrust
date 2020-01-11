5068eabed699db05315174cd9f7c8ff0ac790450
Amit Karande <18024430+amitk-abk@users.noreply.github.com>
2020-01-11 23:00:28 +0530

    Initializing from file

--- lengaburutraffic/src/main/java/com/gt/traffic/Main.java
+++ lengaburutraffic/src/main/java/com/gt/traffic/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         if (args.length == 0)
             throw new RuntimeException("Input file is missing");
-        new TourOfLengaburu("./src/main/resources/initialize.txt").doTour(args[0]);
+        new TourOfLengaburu("/initialize.txt")
+                .doTour(args[0]);
     }
 }
